ed4fe2497cf2cef49106327d3bca688811cc5f71
Prabhat Thapa <akoup66@gmail.com>
2023-07-09 13:00:32 +0545

    feat: flask api routes to trigger mail and display companies

---
+++ main.py
@@ -0,0 +1,32 @@
+import nepali_datetime
+from flask import Flask, render_template, request, redirect, url_for
+from src.ipo_api import Ipo
+
+
+app = Flask(__name__)
+ipo = Ipo()
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # if request.method == 'POST':
+    #     return redirect(url_for('index'), )
+    try:
+        ipo.reformat_opening_date()
+        return render_template(
+            "ipo.html",
+            ipos=ipo.data,
+            date_today=nepali_datetime.datetime.today().strftime('%y-%B-%d')
+        )
+    except Exception as e:
+        return f"{e} occured"
+
+
+@app.route('/send_mail')
+def send_mail():
+    ipo.call_send_mail()
+    return ""
+
+
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
