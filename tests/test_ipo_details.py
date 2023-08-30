a1d5da0edd3cdb18f1e3fa10a44d49bb07594df1
Prabhat Thapa <akoup66@gmail.com>
2023-08-30 11:49:06 +0530

    test: add test for getting data from web

--- tests/test_ipo_details.py
+++ tests/test_ipo_details.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
 
+import src.ipo_details
 from src.ipo_details import Ipo
 
 data = [{'companyid': 1138, 'ratio_value': None, 'total_units': '712220.00', 'issue_price': '116.00', 'opening_date': '2023-08-30', 'closing_date': '2023-09-01', 'final_date': '2023-09-05', 'right_eligibility_link': None, 'announcement_link': 'https://www.sharesansar.com/announcementdetail/bhagawati-hydropower-development-company-limited-has-published-an-offer-letter-to-issue-712220-units-ipo-shares-to-the-general-public-from-bhadra-5-8-2080-2023-08-14', 'issue_manager': 'Siddhartha Capital Ltd', 'listing_date': '', 'displayable_share_type': 'Others', 'company': {'id': 1138, 'symbol': "<a href='https://www.sharesansar.com/company/bhpdcl'>BHPDCL</a>", 'companyname': "<a href='https://www.sharesansar.com/company/bhpdcl'>Bhagawati Hydropower Development Company Limited</a>"}}]
@@ -7,6 +8,10 @@ data = [{'companyid': 1138, 'ratio_value': None, 'total_units': '712220.00', 'is
 
 class TestIpo():
     ipo = Ipo()
+
+    def test_get_data_from_web(self, ):
+        parsed_data = self.ipo.get_data_from_web()
+        assert len(parsed_data) > 10
     # when ipo is opened the today
     def test_check_opening_date(self):
         details = self.ipo.get_ipos_opened_today(data)
