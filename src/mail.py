75e4b1f0c9e7055df977603e6d85b697499d49ef
Prabhat Thapa <akoup66@gmail.com>
2023-09-04 01:36:53 +0530

    feat: use different package to get json data

--- src/mail.py
+++ src/mail.py
@@ -13,13 +13,12 @@ def compose_mail_message(sender_email, password, receivers_mail, symbol, name, c
            f"आज {symbol} कम्पनीको आईपीओ(IPO) खुलेको छ। यस कम्पनीको आईपीओ " \
            f"{str(datetime.date.today())} बाट {closing_date} " \
            f"सम्म खुला रहनेछ। \n\nहार्दिक शुभकामना\nप्रभात थापा\nIPO alerts परिवार"
-    logging.info('[4] Message has been composed')
+    logging.info('[3] Message has been composed')
     send_mail(sender_email, password, receivers_mail, subject, body)
 
 
 def send_mail(sender, password, receivers, subject, body):
     server = gmail_login(sender, password)
-    logging.info("[5] Successfully logged into gmail server")
 
     for receiver in receivers.split(','):
         try:
@@ -30,15 +29,19 @@ def send_mail(sender, password, receivers, subject, body):
             msg.attach(MIMEText(body, 'plain'))
             server.send_message(msg)
         except Exception as e:
+            logging.warning('[5] Couldn\'t send mail')
             return f'Error: {e}'
-    logging.info("[6] Message sent to recipient")
+    logging.error("[5] Message sent to recipient")
     server.quit()
     return "Message sent to recipient"
 
 
 def gmail_login(sender, password):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(sender, password)
-    logging.info("[4] Failed to login invalid credentials")
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender, password)
+        logging.info("[4] Successfully logged into gmail server")
+    except Exception:
+        logging.error("[4] Failed to login invalid credentials")
     return server
